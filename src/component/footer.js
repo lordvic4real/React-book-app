98209dc528599ef3e48b5344240a1ea6f806eab7
lordvic4real <lordvic4real@gmail.com>
2021-01-02 03:08:16 +0100

    worked on footer

--- src/component/footer.js
+++ src/component/footer.js
@@ -4,12 +4,19 @@ import styled from "styled-components";
 const FooterContainer = styled.footer`
   height: 100px;
   background: linear-gradient(rgba(0, 0, 0, 0.7), rgba(4, 4, 4, 0.8));
+  display: grid;
+  place-items: center;
+  color: #fff;
+  font-size: 23px;
+  font-weight: bold;
 `;
 
 export default function Footer() {
   return (
     <>
-      <FooterContainer>HELLO</FooterContainer>
+      <FooterContainer>
+        <span>&copy;2021 christian books</span>
+      </FooterContainer>
     </>
   );
 }
